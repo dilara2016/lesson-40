229b7ea896d1a8a7712faafc625d88b909ce64d3
dilara2016 <m.123.hunain@gmail.com>
2025-06-14 21:29:36 +0200

    Update top window.py

--- top window.py
+++ top window.py
@@ -4,7 +4,7 @@ root.geometry("400x300")
 root.title("main")
 def topwin():
  top = Toplevel()
- top.geometry("180x100")
+ top.geometry("160x100")
  top.title("toplevel")
  l2=Label(top, text = "this  is top level window")
  l2.pack()
